2aca131a5673084dc3aedae6c55c9319a9436505
Koichi Nakayamada <koichi.nakayamada@gmail.com>
2025-01-01 20:06:56 -0800

    commit

--- scripts/copy-link.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const copyButtons = document.querySelectorAll('.copy-link');
-
-    copyButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const link = button.getAttribute('data-link');
-        });
-    });
-});
