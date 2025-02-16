56959696211d254efc66d66efc8349830ea286d0
Koichi Nakayamada <koichi.nakayamada@gmail.com>
2025-02-15 16:08:43 -0800

    Enhance sidebar functionality and improve header behavior on scroll; update project and blog sections for better organization and clarity

--- _site/scripts/header.js
+++ _site/scripts/header.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const hideThreshold = 50;
 
     window.addEventListener("scroll", () => {
+        if (header.classList.contains("open")) return;
         if (window.scrollY > lastScrollY && window.scrollY > hideThreshold) {
             header.classList.add("hide");
         } else if (window.scrollY < lastScrollY) {
