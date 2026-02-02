88d7b82216dfa5520d880ccf14c5e602f5d6d2f8
Koichi Nakayamada <151496024+koichincom@users.noreply.github.com>
2026-02-02 04:48:33 -0800

    refactor(utils): Update readingTime calculation with hours support and fix return type

--- src/utils/readingTime.ts
+++ src/utils/readingTime.ts
@@ -1,6 +1,14 @@
-export function getReadingTime(content: string): number {
-    const WORDS_PER_MINUTE = 200;
+export function getReadingTime(content: string): string {
+    const WORDS_PER_MINUTE = 238;
     const words = content.trim().split(/\s+/).length;
     const minutes = Math.ceil(words / WORDS_PER_MINUTE);
-    return `${minutes} min`;
+    if (minutes > 60) {
+        const hours = Math.trunc(minutes / 60);
+        const remainingMinutes = minutes % 60;
+        return remainingMinutes === 0
+            ? `${hours} hr${hours > 1 ? "s" : ""}`
+            : `${hours} hr${hours > 1 ? "s" : ""} ${remainingMinutes} min`;
+    } else {
+        return `${minutes} min`;
+    }
 }
