c3fc5192d9e57e7581e6f57fb55f89d7cf8c1caa
Koichi Nakayamada <151496024+koichincom@users.noreply.github.com>
2026-03-08 13:38:10 -0700

    refactor(utils): Add social-links utility module

---
+++ src/utils/social-links.ts
@@ -0,0 +1,13 @@
+export const SOCIAL_LINKS = {
+    mastodonProfile: "https://hachyderm.io/@koichi",
+    githubProfile: "https://github.com/koichincom",
+    linkedInProfile: "https://linkedin.com/in/koichincom",
+    signalProfile:
+        "https://signal.me/#eu/hFxJqhdUe_9zSwQs01xjuf6sz9n5Zag3yfHQzxGwPZPL-X863I9DF5QXch7gCsUv",
+    email: "k@koichin.com",
+} as const;
+
+export const SOCIAL_IDENTIFIERS = {
+    fediverseCreator: "@koichi@hachyderm.io",
+    twitterCreator: "@koichincom",
+} as const;
